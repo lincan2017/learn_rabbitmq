6f4c972197f33c2311a45b7831da469010e594bf
lincan <gd_lincan@126.com>
2019-09-24 09:56:31 +0800

    set several consumer consume msg.

--- src/test/java/learn/spring/TestRabbitService.java
+++ src/test/java/learn/spring/TestRabbitService.java
@@ -20,7 +20,7 @@ public class TestRabbitService extends JunitBase {
 
     @Test
     public void testMultiContainerFactory() throws Exception{
-        for (int i=0; i<10; i++) {
+        for (int i=0; i<10000; i++) {
             rabbitService.sendToExchange(RabbitMqContant.DIRECT_EX, RabbitMqContant.SMS_R_KEY,"I am a sms...");
             rabbitService.sendToExchange(RabbitMqContant.DIRECT_EX, RabbitMqContant.EMAIL_R_KEY,"I am a email...");
             rabbitService.sendToExchange(RabbitMqContant.DEAD_EX, RabbitMqContant.DEAD_R_KEY,"I am a dead msg...");
@@ -31,4 +31,18 @@ public class TestRabbitService extends JunitBase {
             System.out.println("休息了" + 10 * (i + 1) + "秒-----------------");
         }
     }
+
+    @Test
+    public void testSeveralConsume() throws Exception{
+
+        for (int i=0; i<100000; i++) {
+            rabbitService.sendToExchange(RabbitMqContant.DIRECT_EX, RabbitMqContant.EMAIL_R_KEY,"I am a email...");
+        }
+
+        for (int i=0; i<10; i++) {
+            Thread.sleep(10000);
+            System.out.println("休息了" + 10 * (i + 1) + "秒-----------------");
+        }
+    }
+
 }
