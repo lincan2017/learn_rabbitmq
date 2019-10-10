32b77480fcf6476ec9470d47cda3100acab3a1fe
lincan <gd_lincan@126.com>
2019-10-10 16:16:51 +0800

    Revert "test git"
    This reverts commit 511895cce051229310c3bcea0ae3a5c258087247.

--- src/main/java/learn/spring/listener/MyMqListener.java
+++ src/main/java/learn/spring/listener/MyMqListener.java
@@ -55,6 +55,5 @@ public class MyMqListener {
         rabbitServiceBo.ack(message,channel,false);
     }
 
-    //test
 
 }
--- src/main/java/learn/spring/listener/TestTest.java
+++
@@ -1,8 +0,0 @@
-package learn.spring.listener;
-
-/**
- * @author : Lin Can
- * @date : 2019/10/10 14:53
- */
-public class TestTest {
-}
